4dead126829722d2337e5af6a69f36274c86795c
chrisJuresh <christian159260@gmail.com>
2026-06-27 03:51:57 +0100

    Add Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,8 @@
+<script>
+  import { dev } from '$app/environment';
+  import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+  injectAnalytics({ mode: dev ? 'development' : 'production' });
+</script>
+
 <slot />
